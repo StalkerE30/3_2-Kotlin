24429af9143bbf1bbaf8f043dc8522a16ed8f57d
StalkerE30 <mpower_e30@rambler.ru>
2022-06-27 09:00:04 +0200

    optimized fun main(), add fun sumRate()

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -25,11 +25,11 @@ fun sumRateVkPay(sumMonth: Int = 0, amount: Int): Int {
 
 fun checkTransferAllCard(
     amountDay: Int = 0,
-    dayLimit: Int,
     sumMonth: Int = 0,
-    limitPullMonth: Int,
     amount: Int,
 ): Boolean {
+    val dayLimit = 150_000_00 //дневной лимит перевода в день по всем картам, кроме VK pay
+    val limitPullMonth = 600_000_00 //месячный лимит на отправку по всем картам, кроме VK pay
     val sumMonthCurr = sumMonth + amount
     return when {
         (amount + amountDay > dayLimit) -> {
@@ -61,22 +61,13 @@ fun checkTransferVKpay(sumMonth: Int = 0, amount: Int): Boolean {
         else -> true
     }
 }
-
-fun main() {
-    val sumMonth: Int = 78000_00 //общая сумма переводов за текущий месяц без учета комиссий
-    val typeCard: String = "Visa" //тип карты отправителя
-    val amount: Int = 500_00 //сумма превода
-    val dayLimit: Int = 150_000_00 //дневной лимит перевода в день по всем картам, кроме VK pay
-    val limitPullMonth = 600_000_00 //месячный лимит на отправку по всем картам, кроме VK pay
-    val amountDay = 1000_00 //сумма переводов за текущий день
+fun sumRate(typeCard: String = "MasterCard" , amountDay: Int = 0, sumMonth: Int =0, amount: Int) {
     var result: Int = -1 //вычисленная комиссия за перевод
-    println("Пытаемся перевести сумму ${amount/100} рублей по карте $typeCard")
-    println("Сумма переводов за сегодня ${amountDay/100} руб, за текущий месяц  ${sumMonth/100} руб")
     when (typeCard) {
-        "MasterCard", "Maestro" -> if (checkTransferAllCard(amountDay, dayLimit, sumMonth, limitPullMonth, amount))
+        "MasterCard", "Maestro" -> if (checkTransferAllCard(amountDay, sumMonth, amount))
             result = sumRateMasterMaestro(sumMonth, amount)
 
-        "Visa", "Мир" -> if (checkTransferAllCard(amountDay, dayLimit, sumMonth, limitPullMonth, amount))
+        "Visa", "Мир" -> if (checkTransferAllCard(amountDay, sumMonth, amount))
             result = sumRateVisaMir(amount)
 
         "Vk Pay" -> if (checkTransferVKpay(sumMonth, amount)) result = sumRateVkPay(sumMonth, amount)
@@ -85,5 +76,15 @@ fun main() {
     if (result >= 0) {
         println("Комиссия за перевод составит: $result копеек")
     } else println("Перевод невозможен!")
+
+}
+fun main() {
+    val sumMonth = 78000_00 //общая сумма переводов за текущий месяц без учета комиссий
+    val amountDay = 1000_00 //сумма переводов за текущий день
+    val typeCard = "Visa" //тип карты отправителя
+    val amount = 500_00 //сумма превода
+    println("Пытаемся перевести сумму ${amount/100} рублей по карте $typeCard")
+    println("Сумма переводов за сегодня ${amountDay/100} руб, за текущий месяц  ${sumMonth/100} руб")
+    sumRate(typeCard, amountDay, sumMonth, amount)
 }
 
